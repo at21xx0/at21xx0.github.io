e68e2e8512b6bbfaba2ca69c8c17198bf1c5ea28
at21xx0 <52802811+at21xx0@users.noreply.github.com>
2019-08-30 15:50:38 +0800

    Update musicj.js

--- musicj.js
+++ musicj.js
@@ -1,11 +1,3 @@
 //API来自 https://github.com/messoer/
 function GetF(url,func){var xmlhttp;if(window.XMLHttpRequest){xmlhttp=new XMLHttpRequest();}else{xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}xmlhttp.onreadystatechange=function(){if(xmlhttp.readyState==4&&xmlhttp.status==200){var value=xmlhttp.responseText;func(value);}};xmlhttp.open("GET",url,true);xmlhttp.send();}
-function play(a){
-GetF("https://v1.itooi.cn/netease/song?id="+a.id,function(data){data=JSON.parse(data);/*alert(JSON.stringify(data));*/
-if(!a.p)a.p="Api..........";
-if(a.src){void(0)}else if(a.m){a.src=a.p+a.q}else{a.src="https://v1.itooi.cn/netease/url?id="+a.id+"&quality=128"}
-var d={title:data.data.songs[0].al.name,pic:data.data.songs[0].al.picUrl,id:data.data.songs[0].al.id,src:a.src};
-if(a.func){a.func(d);}else{musicd.mplay(d);}
-alert(JSON.stringify(d));})
-}
-alert("1就绪");
+function play(a){GetF("https://v1.itooi.cn/netease/song?id="+a.id,function(data){data=JSON.parse(data);/*alert(JSON.stringify(data));*/if(!a.p)a.p="Api..........";if(a.src){void(0)}else if(a.m){a.src=a.p+a.q}else{a.src="https://v1.itooi.cn/netease/url?id="+a.id+"&quality=128"}var d={title:data.data.songs[0].al.name,pic:data.data.songs[0].al.picUrl,id:data.data.songs[0].al.id,src:a.src};if(a.func){a.func(d);}else{musicd.mplay(d);}/*alert(JSON.stringify(d));*/})}
