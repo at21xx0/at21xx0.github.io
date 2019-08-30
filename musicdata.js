c54c8bd47c2fe3ac72d0ec33ef4d8f063d33c374
at21xx0 <52802811+at21xx0@users.noreply.github.com>
2019-08-30 15:46:11 +0800

    Add files via upload

---
+++ musicdata.js
@@ -0,0 +1,9 @@
+var data=
+[{"id":27876224,"name":"God knows"},
+{"id":29732992,"name":"光るなら"},
+{"id":28151022,"name":"鳥の詩"},
+{"id":450041131,"name":"Daydream cafe","src":"m/1.mp3"},
+{"id":26201959,"name":"天使にふれたよ! (相遇天使)"},
+{"id":412327036,"name":"いけないボーダーライン"},
+{"id":672188,"name":"You"},
+{"id":1313052233,"name":"aLIEz (澤野弘之 LIVE[nZk]004 (2016/11/03@TOKYO DOME CITY HALL))"}];
